b3bcdf1b7d6ff189385b0d9675030bb57a31c87f
Dario <dariocovella@gmail.com>
2023-12-06 01:06:48 +0000

    Create item class in model.py of todo app

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Item(model.Model):
+    name = models.Charfield(max_lenght=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
